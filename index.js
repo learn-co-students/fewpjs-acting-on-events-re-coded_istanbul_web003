fac5d662ea8a932339b0a6949ac4ec7a272790bd
ihab-sensei <ib-mashhadani@hotmail.com>
2020-07-24 22:46:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.querySelector("#dodger");
+
+function moveDodgerLeft() {
+    let leftPixelsStr = dodger.style.left.replace("px", "");
+    let leftPixelsInt = parseInt(leftPixelsStr, 10);
+    if (leftPixelsInt > 0) {
+        dodger.style.left = `${leftPixelsInt -1}px`;
+        console.log(dodger.style.left);
+    }
+}
+
+function moveDodgerRight() {
+    
+    let leftPixelsStr = dodger.style.left.replace("px", "");
+    let leftPixelsInt = parseInt(leftPixelsStr, 10);
+    if (leftPixelsInt < 360) {
+        dodger.style.left = `${leftPixelsInt +1}px`;
+        console.log(dodger.style.left);
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key == "ArrowRight") {
+        moveDodgerRight();
+    }
+})
+
+
